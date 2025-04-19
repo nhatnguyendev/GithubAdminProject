00c1f7391cd74ddb60ad366c32ae76a61b633cd6
Nhat Nguyen <nhatnguyendevs@gmail.com>
2025-04-19 16:36:12 +0700

    add user repository tests

---
+++ Modules/Data/Tests/DataTests/UserRepositoryTests.swift
@@ -0,0 +1,76 @@
+//
+//  File.swift
+//  Data
+//
+//  Created by Nhat Nguyen on 18/4/25.
+//
+
+import XCTest
+import Combine
+@testable import Data
+
+final class UserRepositoryTests: XCTestCase {
+    
+    private var cancellables: Set<AnyCancellable> = []
+    
+    final class MockAPIService: APIServiceProtocol {
+        var shouldError = false
+        var mockResponseFilename: String?
+        
+        func request<T>(endpoint: BaseRequest, responseType: T.Type) -> AnyPublisher<T, any Error> where T : Decodable, T : Encodable {
+            if shouldError {
+                return Fail(error: APIError.httpError(statusCode: 500, message: "Interal Error")).eraseToAnyPublisher()
+            } else if let filename = mockResponseFilename, let mockResponse: T = MockDataHelper.loadJSONFile(filename: filename, type: T.self) {
+                return Just(mockResponse)
+                    .setFailureType(to: Error.self)
+                    .eraseToAnyPublisher()
+            } else {
+                return Empty().eraseToAnyPublisher()
+            }
+        }
+    }
+    
+    func test_fetchUsers() {
+        
+        let expectation = self.expectation(description: "Fetch users successfully")
+        
+        let apiService = MockAPIService()
+        apiService.mockResponseFilename = "Users"
+        
+        let userRepository = UserRepository(apiService: apiService)
+        userRepository.fetchUsers(since: 0)
+            .sink { completion in
+                if case .failure(let error) = completion {
+                    XCTFail("Expected success but got error: \(error)")
+                }
+            } receiveValue: { users in
+                XCTAssertEqual(users.count, 2)
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
+        
+        wait(for: [expectation], timeout: 1.0)
+    }
+    
+    func test_fetchUsers_failure() {
+        let expectation = self.expectation(description: "Fetch users fails")
+        
+        let apiService = MockAPIService()
+        apiService.mockResponseFilename = "Users"
+        apiService.shouldError = true
+        
+        let userRepository = UserRepository(apiService: apiService)
+        userRepository.fetchUsers(since: 0)
+            .sink { completion in
+                if case .failure(let error) = completion {
+                    XCTAssertTrue(error is APIError, "Expected error to be of type APIError")
+                    expectation.fulfill()
+                }
+            } receiveValue: { users in
+                XCTFail("Expected failure")
+            }
+            .store(in: &cancellables)
+        
+        wait(for: [expectation], timeout: 1.0)
+    }
+}
