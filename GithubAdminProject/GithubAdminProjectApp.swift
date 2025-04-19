5bf759ee98197827631f8ad714c6c88a3eabb43d
Nhat Nguyen <nhatnguyendevs@gmail.com>
2025-04-19 15:50:49 +0700

    Initial Commit

---
+++ GithubAdminProject/GithubAdminProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubAdminProjectApp.swift
+//  GithubAdminProject
+//
+//  Created by Nhat Nguyen on 19/4/25.
+//
+
+import SwiftUI
+
+@main
+struct GithubAdminProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
