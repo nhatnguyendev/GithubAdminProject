b1d5273d3da75fdd07a3d87e71f0b8b75d014316
Nhat Nguyen <nhatnguyendevs@gmail.com>
2025-04-19 16:28:12 +0700

    add DTO models

---
+++ Modules/Data/Sources/Data/DTOs/UserDTO.swift
@@ -0,0 +1,80 @@
+//
+//  UserDTO.swift
+//  Data
+//
+//  Created by Nhat Nguyen on 19/4/25.
+//
+
+import Domain
+import Foundation
+
+public struct UserDTO: Codable {
+    let login: String?
+    let id: Int?
+    let nodeID: String?
+    let avatarURL: String?
+    let gravatarID: String?
+    let url: String?
+    let htmlURL: String?
+    let followersURL: String?
+    let followingURL: String?
+    let gistsURL: String?
+    let starredURL: String?
+    let subscriptionsURL: String?
+    let organizationsURL: String?
+    let reposURL: String?
+    let eventsURL: String?
+    let receivedEventsURL: String?
+    let type: String?
+    let userViewType: String?
+    let siteAdmin: Bool?
+
+    enum CodingKeys: String, CodingKey {
+        case login
+        case id
+        case nodeID = "node_id"
+        case avatarURL = "avatar_url"
+        case gravatarID = "gravatar_id"
+        case url
+        case htmlURL = "html_url"
+        case followersURL = "followers_url"
+        case followingURL = "following_url"
+        case gistsURL = "gists_url"
+        case starredURL = "starred_url"
+        case subscriptionsURL = "subscriptions_url"
+        case organizationsURL = "organizations_url"
+        case reposURL = "repos_url"
+        case eventsURL = "events_url"
+        case receivedEventsURL = "received_events_url"
+        case type
+        case userViewType = "user_view_type"
+        case siteAdmin = "site_admin"
+    }
+}
+
+public extension UserDTO {
+    func toDomain() -> UserEntity {
+        return UserEntity(
+            login: login ?? "",
+            id: id ?? -1,
+            nodeID: nodeID ?? "",
+            avatarURL: avatarURL ?? "",
+            gravatarID: gravatarID ?? "",
+            url: url ?? "",
+            htmlURL: htmlURL ?? "",
+            followersURL: followersURL ?? "",
+            followingURL: followingURL ?? "",
+            gistsURL: gistsURL ?? "",
+            starredURL: starredURL ?? "",
+            subscriptionsURL: subscriptionsURL ?? "",
+            organizationsURL: organizationsURL ?? "",
+            reposURL: reposURL ?? "",
+            eventsURL: eventsURL ?? "",
+            receivedEventsURL: receivedEventsURL ?? "",
+            type: type ?? "",
+            userViewType: userViewType ?? "",
+            isSiteAdmin: siteAdmin ?? false
+        )
+    }
+}
+
