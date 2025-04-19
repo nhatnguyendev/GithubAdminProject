c05cfead55e2a2d0e0a585fd8b3976b938750a00
Nhat Nguyen <nhatnguyendevs@gmail.com>
2025-04-19 23:04:28 +0700

    add presentation

---
+++ Modules/Presentation/Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Presentation",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Presentation",
+            targets: ["Presentation"]),
+    ],
+    dependencies: [
+        .package(path: "../Data"),
+        .package(path: "../Domain")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Presentation",
+            dependencies: [
+                .product(name: "Data", package: "Data"),
+                .product(name: "Domain", package: "Domain")
+            ]
+        ),
+        .testTarget(
+            name: "PresentationTests",
+            dependencies: ["Presentation"]
+        ),
+    ]
+)
