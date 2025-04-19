2288b96a7a9a7496c6a3f28bdfb0de80c263f42d
Nhat Nguyen <nhatnguyendevs@gmail.com>
2025-04-19 17:33:37 +0700

    add router

---
+++ GithubAdminProject/Router/Destination.swift
@@ -0,0 +1,10 @@
+//
+//  Destination.swift
+//  GithubAdminProject
+//
+//  Created by Nhat Nguyen on 19/4/25.
+//
+
+enum Destination: Hashable {
+    case userDetail(id: String)
+}
---
+++ GithubAdminProject/Router/Router.swift
@@ -0,0 +1,25 @@
+//
+//  Router.swift
+//  GithubAdminProject
+//
+//  Created by Nhat Nguyen on 19/4/25.
+//
+
+import Foundation
+
+final class Router: ObservableObject, RouterProtocol {
+    
+    @Published var path = [Destination]()
+    
+    func navigate(to destination: Destination) {
+        path.append(destination)
+    }
+    
+    func navigateBack() {
+        path.removeLast()
+    }
+    
+    func navigateToRoot() {
+        path.removeLast(path.count)
+    }
+}
